336ffedc13eddec9683b9d07f556c8f8adc0be90
aidilbaihaqi <aidillmusirjun@gmail.com>
2024-04-12 21:11:22 +0700

    Update Dashboard: Card, Data Mahasiswa get count

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,15 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/dashboard', function () {
-    return view('pages/dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Authentication Middleware
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+});
 
+// Authentication Profile Middleware
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
