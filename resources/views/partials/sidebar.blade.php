dbae7adddbfc786ff415a17a3fe052cd961568da
aidilbaihaqi <aidillmusirjun@gmail.com>
2024-04-12 14:01:31 +0700

    Deploy First App

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,28 @@
+<div id="layoutSidenav_nav">
+  <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+      <div class="sb-sidenav-menu">
+          <div class="nav">
+              <div class="sb-sidenav-menu-heading">Core</div>
+              <a class="nav-link" href="{{ route("dashboard") }}">
+                  <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                  Dashboard
+              </a>
+              <div class="sb-sidenav-menu-heading">Tables</div>
+              <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                  <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                  Data
+                  <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+              </a>
+              <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                  <nav class="sb-sidenav-menu-nested nav">
+                      <a class="nav-link" href="layout-static.html">Data Mahasiswa</a>
+                  </nav>
+              </div>
+          </div>
+      </div>
+      <div class="sb-sidenav-footer">
+          <div class="small">Logged in as:</div>
+          Administrator
+      </div>
+  </nav>
+</div>
