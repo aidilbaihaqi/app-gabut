f67827dd134c4ba93598850fb985d0a35e4e2aab
aidilbaihaqi <aidillmusirjun@gmail.com>
2024-04-12 21:26:04 +0700

    Data Mahasiswa and User: update model, factory, migration, databaseseeder, delete mahasiswaseeder

--- database/migrations/2024_04_12_105347_create_mahasiswas_table.php
+++ database/migrations/2024_04_12_105347_create_mahasiswas_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('mahasiswas', function (Blueprint $table) {
             $table->id();
             $table->string('nim')->unique();
+            $table->string('foto')->nullable();
             $table->string('nama');
             $table->enum('jenis_kelamin', ['L', 'P']);
             $table->string('tempat_lahir');
