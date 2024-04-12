3d149e3b142f8725d539046d81638517d7db38e1
aidilbaihaqi <aidillmusirjun@gmail.com>
2024-04-12 18:33:37 +0700

    Data Mahasiswa: create model, migration, factory, seeder and email domain custom (@umrah.ac.id)

---
+++ database/migrations/2024_04_12_105347_create_mahasiswas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mahasiswas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nim')->unique();
+            $table->string('nama');
+            $table->enum('jenis_kelamin', ['L', 'P']);
+            $table->string('tempat_lahir');
+            $table->date('tgl_lahir');
+            $table->text('alamat');
+            $table->string('no_telp');
+            $table->string('email')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+};
